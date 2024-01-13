1d66c8a8f82c77988b6a6187457e8b6eac6a6622
Mohamed Elbahy <ixbahy@gmail.com>
2024-01-13 20:53:40 +0200

    add: more attributes to the event object

--- Project/src/main/java/Events/Event.java
+++ Project/src/main/java/Events/Event.java
@@ -1,2 +1,38 @@
-package Events;public class Event {
+package Events;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@Builder
+public class Event {
+    public  String mainCategory;
+    public  String subCategory;
+    public  String protocol;
+    public  String sourcePort;
+
+    public  String destinationPort;
+    public  String name;
+    public  UUID id ;
+    public String sourceIP;
+    @Setter
+    public String severity;
+    @Setter
+    public boolean authorizedIp=true;
+    public  Event(){}
+    public Event(String[] props){
+        id= UUID.randomUUID();
+        mainCategory=props[0];
+        subCategory=props[1];
+        protocol=props[2];
+        sourceIP=props[3];
+        sourcePort=props[4];
+        destinationPort=props[5];
+        name=props[6];
+    }
+
 }
